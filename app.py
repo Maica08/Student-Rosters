a22f2d5571caf62c2d9c832cdb3db807a3c26d7c
Maica08 <202280193@psu.palawan.edu.ph>
2024-12-12 22:46:25 +0800

    wip: retrieving data at index.html

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, jsonify, request, make_response
 from flask_mysqldb import MySQL
 
-app = Flask("__name__")
+app = Flask(__name__)
 app.config["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_USER"] = "root"
 app.config["MYSQL_PASSWORD"] = ""
@@ -14,7 +14,8 @@ def execute(query, *args):
         cur.execute(query, tuple(args))
     else:
         cur.execute(query)
-    data = cur.fetchall()
+    columns = [col[0] for col in cur.description]
+    data = [dict(zip(columns, row)) for row in cur.fetchall()]
     cur.close()
     
     return data
@@ -31,7 +32,24 @@ def commit(query, *args):
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    query = """
+        SELECT 
+            classes.description AS class,
+            class_period AS period,
+            courses.name AS course,
+            CONCAT(teachers.firstname, ' ', teachers.lastname) AS teacher,
+            rooms.location AS room
+        FROM classes
+        INNER JOIN rooms ON idrooms = idroom
+        INNER JOIN courses ON idcourses = idcourse
+        INNER JOIN roster ON idclasses = idclass
+        INNER JOIN teachers ON idteachers = idteacher
+        GROUP BY classes.description, class_period, courses.name, rooms.location, teacher
+        ORDER BY classes.description;
+    """
+    
+    results = execute(query)
+    return render_template('index.html', results=results)
 
 @app.route("/students", methods=["GET"])
 def get_books():
